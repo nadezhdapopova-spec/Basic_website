3368af83ed4992191cc8b7bcd3c906603824802c
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-09-10 20:31:37 +0700

    created src/class_server.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from http.server import HTTPServer
+from src.class_server import Server
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    host_name = os.getenv("HOST_NAME", "127.0.0.1")
+    server_port = int(os.getenv("SERVER_PORT", 8080))
+    web_server = HTTPServer((host_name, server_port), Server)
+    print("Server started http://%s:%s" % (host_name, server_port))
+
+    try:
+        web_server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    web_server.server_close()
+    print("Server stopped.")
+
+
+main()
