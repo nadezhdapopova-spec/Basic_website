069dd9b3d6b56b36b8c34bc426513289b2d81f59
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-09-12 01:37:59 +0700

    modified html files, src/class_server/py

--- src/class_server.py
+++ src/class_server.py
@@ -1,3 +1,4 @@
+import os.path
 import urllib
 from http.server import BaseHTTPRequestHandler
 
@@ -7,9 +8,22 @@ class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         """Метод для обработки входящих GET-запросов"""
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+
+        filename = "/page_contacts.html"
+
+        if self.path[-4:] == '.css':
+            self.send_header('Content-type', 'text/css')
+            filename = self.path
+        # elif self.path[-3:] == '.js':
+        #     self.send_header('Content-type', 'application/javascript')
+        #     filename = self.path
+        # elif self.path[-5:] == '.json':
+        #     self.send_header('Content-type', 'application/javascript')
+        #     filename = self.path
+        else:
+            self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.read_html()
+        self.read_content(filename)
 
     def do_POST(self):
         """Метод для обработки POST-запросов"""
@@ -21,9 +35,10 @@ class Server(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
 
-    def read_html(self):
+    def read_content(self, file_name):
         try:
-            with open("dist/page_contacts.html", "r", encoding="utf-8") as file:
+            content_path = "dist" + file_name
+            with open(content_path, "r", encoding="utf-8") as file:
                 content = file.read()
             self.wfile.write(bytes(content, "utf-8"))
         except FileNotFoundError:
